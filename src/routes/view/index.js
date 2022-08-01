c58efab8ab3092d484ec14a3b906a7bda64e1b0c
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-08-01 23:32:52 +0100

    auth mw implemented on private routes

--- src/routes/view/index.js
+++ src/routes/view/index.js
@@ -2,9 +2,10 @@ const { Router } = require('express');
 
 const publicRoutes = require('./publicRoutes');
 const privateRoutes = require('./privateRoutes');
+const auth = require('../../middlewares/auth');
 
 const router = Router();
 
 router.use(publicRoutes);
-router.use(privateRoutes);
+router.use(auth, privateRoutes);
 module.exports = router;
