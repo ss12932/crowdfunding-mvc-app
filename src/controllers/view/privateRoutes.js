b423d1db65e5bcb199f04593f29d3bbc94e974d7
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-08-02 23:52:51 +0100

    finished profile page + link in private navbar

--- src/controllers/view/privateRoutes.js
+++ src/controllers/view/privateRoutes.js
@@ -1,8 +1,27 @@
+const { Project, User } = require('../../models');
+
 const renderCreateProjectPage = (req, res) => {
   return res.render('createProjectPage');
 };
-const renderProfilePage = (req, res) => {
-  return res.render('profile');
+const renderProfilePage = async (req, res) => {
+  const { loggedIn, user } = req.session;
+  const projectsFromDB = await Project.findAll({
+    where: {
+      user_id: user.id,
+    },
+    include: [
+      {
+        model: User,
+        attributes: ['name', 'email'],
+      },
+    ],
+  });
+  const projects = projectsFromDB.map((project) =>
+    project.get({ plain: true })
+  );
+
+  console.log(projects);
+  return res.render('profile', { loggedIn, user, projects });
 };
 module.exports = {
   renderCreateProjectPage,
